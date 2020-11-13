9050f874d633a424fdfcf906b355df614e1b70ef
moofarry <jhon.moofarry00@usc.edu.co>
2020-11-13 01:28:59 -0500

    styles counterApp

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import PrimeraApp from './PrimeraApp'
 import CounterApp from './CounterApp'
-import './index.css'
+import './css/index.css'
 
 //const saludo = <h1> Hola Mundo</h1> ;
 const divRoot = document.querySelector('#root');
